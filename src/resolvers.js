35db9282ad5f8aefa03bf007fec91cc9418b2dfe
Roberto Garcia <92rober@gmail.com>
2024-06-29 00:26:27 +0200

    feat: allow query for particular city

--- src/resolvers.js
+++ src/resolvers.js
@@ -2,6 +2,12 @@ const db = require('./db');
 
 const root = {
   cities: () => db.cities,
+  city: (args) => {
+    if (args.id !== undefined) {
+      return db.cities.find(city => city.id == args.id);
+    }
+    return null;
+  },
   places: () => db.places,
 };
 
--- src/schema.js
+++ src/schema.js
@@ -23,6 +23,7 @@ const schema = buildSchema(`
 
   type Query {
     cities: [City]
+    city(id: Int): City
     places: [Place]
   }
 `);
